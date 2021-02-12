104af3306beabe8d57eb43a340518247184d6eef
He Wang <He.Wang@noaa.gov>
2021-02-11 23:31:18 -0500

    Add Htot diag

--- src/core/MOM_barotropic.F90
+++ src/core/MOM_barotropic.F90
@@ -690,6 +690,8 @@ subroutine btstep(U_in, V_in, eta_in, dt, bc_accel_u, bc_accel_v, forces, pbce, 
   integer :: isd, ied, jsd, jed, IsdB, IedB, JsdB, JedB
   integer :: ioff, joff
   integer :: l_seg
+  character(240) :: msg
+  integer :: ig, jg
 
   if (.not.associated(CS)) call MOM_error(FATAL, &
       "btstep: Module MOM_barotropic must be initialized before it is used.")
@@ -1466,6 +1468,15 @@ subroutine btstep(U_in, V_in, eta_in, dt, bc_accel_u, bc_accel_v, forces, pbce, 
       bt_rem_v(i,J) = bt_rem_v(i,J) * (Htot / (Htot + CS%lin_drag_v(i,J) * dtbt))
 
       Rayleigh_v(i,J) = CS%lin_drag_v(i,J) / Htot
+      if (abs(Htot) < 1e-7) then
+        ig = i + G%HI%idg_offset ! Global i-index
+        jg = j + G%HI%jdg_offset ! Global j-index
+        write(msg(1:240),'(2(a,i4,x),4(a,f8.3,x))') &
+        'Small Htot: i=',ig,'j=',jg, &
+        'eta-=',eta(i,j), 'eta+=',eta(i,j+1), &
+        'bathyT-=',CS%bathyT(i,j), 'bathyT+=',CS%bathyT(i,j+1)
+        call MOM_error(WARNING, trim(msg), all_print=.true.)
+      endif
     endif ; enddo ; enddo
   endif
 
