7c9bbd3622423ef49ea49cb04de7ee5b35d238d1
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2025-08-01 12:56:41 -0400

    Moved hchksum() with halos to after pass_var
    When debugging layout non-reproducible problems, this hchksum showed
    up as having changes in the halo BUT it is immediately followed by
    a pass_var(). Either we need to only check the C-domain or do hchksum
    with halos after the pass_var().

--- src/core/MOM.F90
+++ src/core/MOM.F90
@@ -1388,9 +1388,9 @@ subroutine step_MOM_dynamics(forces, p_surf_begin, p_surf_end, dt, dt_tr_adv, &
       call thickness_diffuse(h, CS%uhtr, CS%vhtr, CS%tv, dt, G, GV, US, &
                              CS%MEKE, CS%VarMix, CS%CDp, CS%thickness_diffuse_CSp, CS%stoch_CS)
 
-      if (CS%debug) call hchksum(h,"Post-thickness_diffuse h", G%HI, haloshift=1, unscale=GV%H_to_MKS)
       call cpu_clock_end(id_clock_thick_diff)
       call pass_var(h, G%Domain, clock=id_clock_pass, halo=max(2,CS%cont_stencil))
+      if (CS%debug) call hchksum(h,"Post-thickness_diffuse h", G%HI, haloshift=1, unscale=GV%H_to_MKS)
       if (showCallTree) call callTree_waypoint("finished thickness_diffuse (step_MOM)")
     endif
 
