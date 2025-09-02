873847827f08335d82483634600a91ff0d9752f1
He Wang <He.Wang@noaa.gov>
2025-09-02 15:16:42 -0400

    add zonal_adjust newton debug

--- src/core/MOM_continuity_PPM.F90
+++ src/core/MOM_continuity_PPM.F90
@@ -1223,7 +1223,7 @@ subroutine zonal_flux_adjust(u, h_in, h_W, h_E, uhbt, uh_tot_0, duhdu_tot_0, &
     do I=ish-1,ieh
     if ((i + G%HI%idg_offset == CS%i_tgt) .and. (j + G%HI%jdg_offset == CS%j_tgt)) then
       write(mesg, *) 'DEBUG cont zonal_flux_adjust: itt, do_I(I), u(i,j,k), du_prev, du(i), u+du', &
-       itt, do_I(I), u(i,j,k), du_prev, du(i), u(i,j,k)+du(i)
+       itt, do_I(I), u(i,j,1), du_prev, du(i), u(i,j,1)+du(i)
       call MOM_error(WARNING, trim(mesg), all_print=.true.)
     endif
     enddo
