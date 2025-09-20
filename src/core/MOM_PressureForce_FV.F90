06f66f1e558b15e1cd69c2733aab84a721e762c8
He Wang <He.Wang@noaa.gov>
2025-09-20 00:44:48 -0400

    Replace pressure_FV SAL
    * SAL use SSH as input, floodable cells

--- src/core/MOM_PressureForce_FV.F90
+++ src/core/MOM_PressureForce_FV.F90
@@ -436,8 +436,9 @@ subroutine PressureForce_FV_nonBouss(h, tv, PFu, PFv, G, GV, US, CS, ALE_CSp, AD
     else
       !$OMP parallel do default(shared)
       do j=Jsq,Jeq+1 ; do i=Isq,Ieq+1
-        SSH(i,j) = (za(i,j,1) - alpha_ref*p(i,j,1)) * I_gEarth - G%Z_ref &
-                  - max(-G%bathyT(i,j)-G%Z_ref, 0.0)
+        SSH(i,j) = (za(i,j,1) - alpha_ref*p(i,j,1)) * I_gEarth - G%Z_ref
+        if (G%meanThick(i,j)==0) & ! Remove above sea level topography at floodable cells
+          SSH(i,j) = SSH(i,j) + (G%bathyT(i,j) + G%Z_ref)
       enddo ; enddo
       call calc_SAL(SSH, e_sal, G, CS%SAL_CSp, tmp_scale=US%Z_to_m)
     endif
@@ -1355,7 +1356,9 @@ subroutine PressureForce_FV_Bouss(h, tv, PFu, PFv, G, GV, US, CS, ALE_CSp, ADp, 
     else
       !$OMP parallel do default(shared)
       do j=Jsq,Jeq+1 ; do i=Isq,Ieq+1
-        SSH(i,j) = e(i,j,1) - max(-G%bathyT(i,j) - G%Z_ref, 0.0) ! Remove topography above sea level
+        SSH(i,j) = e(i,j,1)
+        if (G%meanThick(i,j)==0) & ! Remove above sea level topography at floodable cells
+          SSH(i,j) = SSH(i,j) + (G%bathyT(i,j) + G%Z_ref)
       enddo ; enddo
       call calc_SAL(SSH, e_sal, G, CS%SAL_CSp, tmp_scale=US%Z_to_m)
     endif
