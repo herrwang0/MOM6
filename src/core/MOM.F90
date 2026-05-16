5dc2f0d13d99fbb83e677e77c83fb72f14cd803e
He Wang <He.Wang@noaa.gov>
2026-05-15 21:59:05 -0400

    add chksum

--- src/core/MOM.F90
+++ src/core/MOM.F90
@@ -975,7 +975,7 @@ subroutine step_MOM(forces_in, fluxes_in, sfc_state, Time_start, time_int_in, CS
       endif
 
       if (do_advection) then ! Do advective transport and lateral tracer mixing.
-        call chksum_OBC_segments(CS%OBC, G, GV, US, nk=3, mesg='Pre-tracer_dyn', do_dynamics=.false., do_tracer=.true.)
+        call chksum_OBC_segments(CS%OBC, G, GV, US, nk=3, mesg='Pre-tracer_dyn', do_dynamics=.false., do_tracers=.true.)
         call step_MOM_tracer_dyn(CS, G, GV, US, h, Time_local)
         if (CS%diabatic_first .and. abs(CS%t_dyn_rel_thermo) > 1e-6*dt) call MOM_error(FATAL, &
                 "step_MOM: Mismatch between the dynamics and diabatic times "//&
--- src/core/MOM_dynamics_split_RK2.F90
+++ src/core/MOM_dynamics_split_RK2.F90
@@ -806,9 +806,9 @@ subroutine step_MOM_dyn_split_RK2(u_inst, v_inst, h, tv, visc, Time_local, dt, f
     if (CS%debug) &
       call uvchksum("Pre OBC avg [uv]", u_av, v_av, G%HI, haloshift=1, symmetric=sym, unscale=US%L_T_to_m_s)
 
-    call chksum_OBC_segments(CS%OBC, G, GV, US, nk=3, mesg='Pre-rad', do_dynamics=.false., do_tracer=.true.)
+    call chksum_OBC_segments(CS%OBC, G, GV, US, nk=3, mesg='Pre-rad', do_dynamics=.false., do_tracers=.true.)
     call radiation_open_bdry_conds(CS%OBC, u_av, u_old_rad_OBC, v_av, v_old_rad_OBC, G, GV, US, dt_pred)
-    call chksum_OBC_segments(CS%OBC, G, GV, US, nk=3, mesg='Post-rad', do_dynamics=.false., do_tracer=.true.)
+    call chksum_OBC_segments(CS%OBC, G, GV, US, nk=3, mesg='Post-rad', do_dynamics=.false., do_tracers=.true.)
 
     if (CS%debug) &
       call uvchksum("Post OBC avg [uv]", u_av, v_av, G%HI, haloshift=1, symmetric=sym, unscale=US%L_T_to_m_s)
